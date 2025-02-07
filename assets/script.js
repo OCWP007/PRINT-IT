4cf563475ade1473a87d4bde948e7ec8f00b1845
David Guitou <david_guitou@yahoo.fr>
2025-02-07 04:37:42 +0100

    Mofifs presentation

--- assets/script.js
+++ assets/script.js
@@ -19,36 +19,26 @@ const slides = [
 
 let indice = 0;
 let banner = document.querySelector("#banner");
-//banner.setAttribute("src", slides[indice]);
-console.log(banner);
+//console.log(banner);
 
 let bannerimg = document.querySelector("#banner img");
 bannerimg.setAttribute("src", "./assets/images/slideshow/"+ slides[indice].image);
 banner.setAttribute("p", slides[indice].tagLine);
 console.log(bannerimg);
 
-let tagLineElement = banner.querySelector("p"); // Sélectionner le <p> à l'intérieur de #banner
+let tagLineElement = banner.querySelector("p"); 
 
 let baliseImager = document.createElement("img");
-//let baliseImage = document.getElementById("premiereImage");
 baliseImager.setAttribute("src", "./assets/images/arrow_right.png");
 baliseImager.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
 baliseImager.setAttribute("class", "arrow_right");
-//banner.appendChild(nouvelElement);
 banner.appendChild(baliseImager);
 
 let baliseImagel = document.createElement("img");
-//let baliseImage = document.getElementById("premiereImage");
 baliseImagel.setAttribute("src", "./assets/images/arrow_left.png");
 baliseImagel.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
 baliseImagel.setAttribute("class", "arrow_left");
-//banner.appendChild(nouvelElement);
 banner.appendChild(baliseImagel);
-
-// Création des bullets points
-//const dotsContainer = document.createElement("div");
-//dotsContainer.classList.add("dots");
-//banner.appendChild(dotsContainer);
 
 //----------------------------------------------------- Ajout des bullets points ----------------------
 //récupération de la classe des bullets points pour les positionner sur l'image
@@ -60,26 +50,19 @@ for (let i = 0; i< slides.length; i++) {
 	dot.classList.add("dot");	
 	dots.appendChild(dot);	
 	
-	//SI LE POINT CORRESPOND A LA PREMIERE IMAGE DU TABLEAU(i=0) ALORS ON LE MET BLANC
-	//if (i === 0) {
-	//	dot.classList.add("dot_selected");
-	//}
-
-	// Ajout d'un gestionnaire d'événements pour chaque bullet point
+// Ajout d'un listener pour chaque bullet point
     dot.addEventListener("click", () => {
-        indice = i; // Met à jour l'indice en fonction du bullet cliqué
-        changeban(); // Change l'image et le texte
+        indice = i; 
+        changeban(); 
     });
-
-
 }
 
 function changedots() {
 	const dotspoints = document.querySelectorAll(".dot");
     dotspoints.forEach((dot, index) => {
-        dot.classList.remove("dot_selected"); // Retire la classe sélectionnée de tous les dots
+        dot.classList.remove("dot_selected"); 
         if (index === indice) {
-            dot.classList.add("dot_selected"); // Ajoute la classe sélectionnée au dot correspondant
+            dot.classList.add("dot_selected"); 
         }
     });
 }
@@ -88,10 +71,7 @@ function changeban() {
 	if (indice === slides.length) { indice = 0 };
 	if (indice < 0) { indice = (slides.length - 1) };
 	bannerimg.setAttribute("src", "./assets/images/slideshow/" + slides[indice].image);
-	tagLineElement.innerHTML = slides[indice].tagLine; // Utilisez innerHTML pour les balises HTML
-
-	// Gérer le cycle de l'indice
-    //indice = (indice + 1) % slides.length; // Incrémente l'indice et le remet à 0 si nécessaire
+	tagLineElement.innerHTML = slides[indice].tagLine; 
 	changedots();
 
 }
@@ -117,7 +97,7 @@ baliseImagel.addEventListener("click", () => {
 
 // Défilement automatique toutes les 10 secondes (10000 ms)
 setInterval(() => {
-    indice = (indice + 1) % slides.length; // Incrémente l'indice et le remet à 0 si nécessaire
+    indice = (indice + 1) % slides.length; 
     changeban();
 }, 10000);
 
